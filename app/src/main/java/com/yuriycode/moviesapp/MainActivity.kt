7024af7c6bb59c853acd33145983788b33547e7b
Yuriy Kravets <zoidthe311@gmail.com>
2022-06-14 12:42:27 +0300

    Commit special

---
+++ app/src/main/java/com/yuriycode/moviesapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.yuriycode.moviesapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.yuriycode.moviesapp.navigation.SetupNavHost
+import com.yuriycode.moviesapp.ui.theme.MoviesAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState:Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MoviesAppTheme {
+
+                val navController = rememberNavController()
+                SetupNavHost(navController = navController)
+
+                }
+            }
+        }
+    }
