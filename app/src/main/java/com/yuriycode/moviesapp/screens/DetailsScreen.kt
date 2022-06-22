230275167a001714851c0c2513ed40aa899f6b2c
Yuriy Kravets <zoidthe311@gmail.com>
2022-06-22 14:06:23 +0300

    Edit navHost and edit text for movieDetails screen.

--- app/src/main/java/com/yuriycode/moviesapp/screens/DetailsScreen.kt
+++ app/src/main/java/com/yuriycode/moviesapp/screens/DetailsScreen.kt
@@ -1,13 +1,83 @@
 package com.yuriycode.moviesapp.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
 import com.yuriycode.moviesapp.MainViewModel
+import com.yuriycode.moviesapp.utils.HtmlText
 
 @Composable
-fun DetailsScreen(navController:NavController, viewModel:MainViewModel, itemId: String) {
-    Text("Details Screen: item id ${itemId}")
+fun DetailsScreen(viewModel: MainViewModel, itemId: String) {
+    val currentItem = viewModel.allMovies
+        .observeAsState(listOf()).value
+        .firstOrNull { it.id == itemId.toInt() }
 
-
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(vertical = 24.dp, horizontal = 8.dp)
+    ) {
+        LazyColumn{
+            item {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        painter = rememberImagePainter(currentItem?.image?.medium),
+                        contentDescription = null,
+                        modifier = Modifier.size(512.dp)
+                    )
+                    Text(
+                        text = currentItem?.name ?: "",
+                        fontWeight = FontWeight.ExtraBold,
+                        fontSize = 35.sp,
+                        modifier = Modifier.padding(top = 20.dp)
+                    )
+                    Row (
+                        modifier = Modifier.padding(top = 8.dp)
+                    ){
+                        Text(
+                            text = "Рейтинг: ",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 18.sp
+                        )
+                        Text(
+                            text = currentItem?.rating?.average.toString(),
+                            fontSize = 18.sp
+                        )
+                    }
+                    Row(
+                        modifier = Modifier.padding(top = 8.dp)
+                    ) {
+                        Text(
+                            text = "Жанр: ",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 18.sp
+                        )
+                        currentItem?.genres?.take(2)?.forEach {
+                            Text(
+                                text = " [$it] ",
+                                fontSize = 18.sp
+                            )
+                        }
+                    }
+                    HtmlText(
+                        html = currentItem?.summary ?: "",
+                        modifier = Modifier.padding(top = 15.dp)
+                    )
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/yuriycode/moviesapp/utils/Constants.kt
+++ app/src/main/java/com/yuriycode/moviesapp/utils/Constants.kt
@@ -1,5 +1,11 @@
 package com.yuriycode.moviesapp.utils
 
+import android.widget.TextView
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.text.HtmlCompat
+
 class Constants {
 
     object Screens {
@@ -7,5 +13,13 @@ class Constants {
         const val MAIN_SCREEN = "main_screen"
         const val DETAILS_SCREEN = "details_screen"
     }
+}
 
+@Composable
+fun HtmlText(html: String, modifier: Modifier = Modifier) {
+    AndroidView(
+        modifier = Modifier,
+        factory = {context -> TextView(context) },
+        update = { it.text = HtmlCompat.fromHtml(html, HtmlCompat.FROM_HTML_MODE_COMPACT) }
+    )
 }
